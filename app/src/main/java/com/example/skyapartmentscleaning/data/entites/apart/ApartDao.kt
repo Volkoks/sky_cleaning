7223cfd34dfa3f1b93372db8538ebfcda55ff376
Volkoks <volk-2390@gmail.com>
2020-10-31 21:22:40 +0300

    Добавил интерфейс Дао для Apart

---
+++ app/src/main/java/com/example/skyapartmentscleaning/data/entites/apart/ApartDao.kt
@@ -0,0 +1,19 @@
+package com.example.skyapartmentscleaning.data.entites.apart
+
+import androidx.room.*
+
+@Dao
+interface ApartDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addApart(apart: Apart)
+
+    @Delete
+    fun deleteApart(apart: Apart)
+
+    @Query("SELECT * FROM apart")
+    fun getAllApart(): List<Apart>
+
+    @Query("SELECT COUNT() FROM apart")
+    fun getCountApart(): Long
+
+}

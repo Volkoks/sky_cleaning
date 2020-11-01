39ac3359597f3652b17a3e613902f3addb2fe403
Volkoks <volk-2390@gmail.com>
2020-11-01 15:27:08 +0300

    Дабавил файл с константами (мне кажется так более гибко можно добавлять некоторые значения)

---
+++ app/src/main/java/com/example/skyapartmentscleaning/data/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.skyapartmentscleaning.data
+
+import com.example.skyapartmentscleaning.R
+
+
+const val DONE:String = R.string.done.toString()
+const val NOT_DONE:String = R.string.nor_done.toString()

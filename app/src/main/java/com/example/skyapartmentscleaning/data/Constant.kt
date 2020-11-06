01ec81d9243eee4212c15026dbf683acac1d01f3
Volkoks <volk-2390@gmail.com>
2020-11-06 12:39:26 +0300

    В файле констант приложения изменил варианты записи Выполнено и Не выполнено: заменил на галочку и крестик

--- app/src/main/java/com/example/skyapartmentscleaning/data/Constant.kt
+++ app/src/main/java/com/example/skyapartmentscleaning/data/Constant.kt
@@ -1,12 +1,10 @@
 package com.example.skyapartmentscleaning.data
 
-import com.example.skyapartmentscleaning.R
-
-
-const val DONE:String = "Выполнено"
-const val NOT_DONE:String = "Не выполнено"
+const val DONE:String = "✔"
+const val NOT_DONE:String = "Х"
 const val DIRTY:String = "Грязные"
 const val NORMAL:String = "Норм"
 const val AVAILABLE = "Имеются"
 const val NOT_AVAILABLE = "Не имеются"
+
 

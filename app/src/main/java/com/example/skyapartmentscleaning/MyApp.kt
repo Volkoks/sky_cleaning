0546bd2f90cc9270f32b34600980dfd10255ffc5
Volkoks <volk-2390@gmail.com>
2020-11-10 16:01:46 +0300

    Добавил создание Cicerone в класс App и изменил доступ к БД

--- app/src/main/java/com/example/skyapartmentscleaning/MyApp.kt
+++ app/src/main/java/com/example/skyapartmentscleaning/MyApp.kt
@@ -2,19 +2,36 @@ package com.example.skyapartmentscleaning
 
 import android.app.Application
 import com.example.skyapartmentscleaning.data.entites.apart.ApartDatabase
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.Router
 
 /**
  * @author Alexander Volkov (Volkoks)
  */
 class MyApp : Application() {
 
-    companion object{
-    lateinit var apartDB: ApartDatabase
+    companion object {
+        lateinit var instance: MyApp
+    }
+
+    private val apartDB: ApartDatabase by lazy {
+        ApartDatabase.getInstance(this)
+    }
+    private val cicerone: Cicerone<Router> by lazy {
+        Cicerone.create()
     }
 
     override fun onCreate() {
         super.onCreate()
-        apartDB = ApartDatabase.getInstance(this)
+        instance = this
+
     }
+
+    val getDB
+        get() = apartDB
+    val getRouter
+        get() = cicerone.router
+    val getNavigatorHolder
+    get() = cicerone.navigatorHolder
 
 }

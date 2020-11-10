c496090f2f9d3f3a35f0de79756dcdecd91b33c0
Volkoks <volk-2390@gmail.com>
2020-11-10 17:59:58 +0300

    Добавил навигатор и навигаторХолдер в MainActivity.kt

--- app/src/main/java/com/example/skyapartmentscleaning/MainActivity.kt
+++ app/src/main/java/com/example/skyapartmentscleaning/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.skyapartmentscleaning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.skyapartmentscleaning.ui.main.MainFragment
+import ru.terrakok.cicerone.android.support.SupportAppNavigator
+
 /**
  * @author Alexander Volkov (Volkoks)
  */
 class MainActivity : AppCompatActivity() {
+
+    private val  navigatorHolder = MyApp.instance.getNavigatorHolder
+    private val navigator = SupportAppNavigator(this, supportFragmentManager, R.id.fragment_container)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +22,16 @@ class MainActivity : AppCompatActivity() {
         val mainFragment = MainFragment.newInstance()
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, mainFragment)
             .commit()
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navigatorHolder.removeNavigator()
     }
 
 }
